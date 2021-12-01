1f70cee07bfce297df5d302b46ea62e8d2c9ccba
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 09:34:57 -0600

    Add function to display exercises to HomePage.js

--- lubranoa_react/src/pages/HomePage.js
+++ lubranoa_react/src/pages/HomePage.js
@@ -1,9 +1,17 @@
 import React from "react";
+import ExerciseTable from "../components/ExerciseTable";
+import Navigation from '../components/Navigation';
+import { useState } from 'react';
 
 function HomePage() {
+    
+    const [exercises, setExercises] = useState([]);
+    
     return (
         <>
             <h2>List of Exercises</h2>
+            < ExerciseTable exercises={exercises} />
+            < Navigation />
         </>
     )
 }
