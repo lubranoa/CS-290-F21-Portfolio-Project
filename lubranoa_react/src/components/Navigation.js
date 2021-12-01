684bb9fb444946cc61ee002e3cb6a7d0110c9c9a
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 09:12:13 -0600

    Add routes to Navigation.js and remove h3

--- lubranoa_react/src/components/Navigation.js
+++ lubranoa_react/src/components/Navigation.js
@@ -4,9 +4,8 @@ import { Link } from 'react-router-dom';
 function Navigation() {
     return (
         <nav>
-            <h3>Links</h3>
-            <Link to=''>Go back to Home</Link>
-            <Link to=''>Add an exercise</Link>
+            <Link to='/'>Go back to Home</Link>
+            <Link to='/add-movie'>Add an exercise</Link>
         </nav>
     );
 }
