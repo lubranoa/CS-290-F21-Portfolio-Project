3df9ce31c32adccdf02ef5a6a8b83545eb8b660f
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 10:02:16 -0600

    Add loadExercises function to pull data from MongoDB

--- lubranoa_react/src/pages/HomePage.js
+++ lubranoa_react/src/pages/HomePage.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ExerciseTable from "../components/ExerciseTable";
 import Navigation from '../components/Navigation';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function HomePage() {
     
     const [exercises, setExercises] = useState([]);
+
+    const loadExercises = async () => {
+        const response = await fetch('/exercises');
+        const data = await response.json();
+        setExercises(data);
+    }
+
+    useEffect(() => {
+        loadExercises();
+    }, []);
     
     return (
         <>
