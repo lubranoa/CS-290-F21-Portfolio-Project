fb63160b65533670b706f4fef8cf0e6fd5f8ed02
lubranoa <lubranoa@oregonstate.edu>
2021-11-30 21:22:23 -0600

    Define exercise schema and model

--- lubranoa_rest/exercises_model.mjs
+++ lubranoa_rest/exercises_model.mjs
@@ -10,3 +10,19 @@ const db = mongoose.connection;
 db.once('open', () => {
     console.log('Successfully connected to MongoDB using Mongoose!');
 });
+
+/**
+ * Define the exercise schema
+ */
+ const exerciseSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    reps: { type: Number, required: true },
+    weight: { type: Number, required: true },
+    unit: { type: String, required: true },
+    date: { type: String, required: true },
+});
+
+/**
+ * Compile the model from the schema
+ */
+ const Exercise = mongoose.model('Exercise', exerciseSchema);
