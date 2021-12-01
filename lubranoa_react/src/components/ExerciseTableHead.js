0fe33d5c3de1756cad16a8862ef13fffe46655e6
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 08:54:08 -0600

    Fix wrong file for ExerciseTableHead function

---
+++ lubranoa_react/src/components/ExerciseTableHead.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ExerciseTableHead() {
+    return (
+        <thead>
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+    );
+}
+
+export default ExerciseTableHead;
