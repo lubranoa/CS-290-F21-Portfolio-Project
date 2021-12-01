f295162263c421143647db8923b39e2081e07222
lubranoa <lubranoa@oregonstate.edu>
2021-11-30 22:16:46 -0600

    Add route handler for creating exercises using POST

--- lubranoa_rest/exercises_controller.mjs
+++ lubranoa_rest/exercises_controller.mjs
@@ -5,3 +5,19 @@ const PORT = 3000;
 
 const app = express();
 app.use(express.json());
+
+/**
+ * Create a new exercise using the name, reps, weight, unit, 
+ * and date provided in the body
+ */
+ app.post('/exercises', (req, res) => {
+    exercises.createExercise(req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date)
+        .then(exercise => {
+            res.status(201).json(exercise);
+        })
+        .catch(error => {
+            console.error(error);
+            // In case of an error, send back status code 400.
+            res.status(400).json({ Error: 'Request failed' });
+        });
+});
