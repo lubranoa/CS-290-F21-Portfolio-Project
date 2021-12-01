a699101bc4b311ee42914a95b754925dc5f19ec9
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 08:41:08 -0600

    Add boilerplate code to ExerciseRow.js

---
+++ lubranoa_react/src/components/ExerciseRow.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Exercise ({ exercise }) {
+    return(
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    );
+}
+
+export default Exercise;
