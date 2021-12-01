960f9ca145331cfe0860e1c054b346cfdf7661fa
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 07:33:46 -0600

    Add imports and route structure to App.js

--- lubranoa_react/src/App.js
+++ lubranoa_react/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AddExercisePage from './pages/AddExercisePage';
+import EditExercisePage from './pages/EditExercisePage';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="App-header">
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/add-movie">
+            <AddExercisePage />
+          </Route>
+          <Route path="/edit-movie">
+            <EditExercisePage />
+          </Route>
+          </div>
+      </Router>
     </div>
   );
 }
