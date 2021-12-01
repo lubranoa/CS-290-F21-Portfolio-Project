6090a578cc06d832f21469236103347ae824f761
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 08:04:53 -0600

    Add boilerplate code to AddExercisePage.js

---
+++ lubranoa_react/src/pages/AddExercisePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddExercisePage = () => {
+
+    return (
+        <div>
+            <h1>Add Exercise</h1>
+        </div>
+    );
+}
+
+export default AddExercisePage;
