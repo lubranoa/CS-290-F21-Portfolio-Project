d928d54aa10b6e8589c3117699f7a10885c44458
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 09:17:48 -0600

    Add properties to Exercise.js

--- lubranoa_react/src/components/Exercise.js
+++ lubranoa_react/src/components/Exercise.js
@@ -3,13 +3,13 @@ import React from "react";
 function Exercise ({ exercise }) {
     return(
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>{ exercise.name }</td>
+            <td>{ exercise.reps }</td>
+            <td>{ exercise.weight }</td>
+            <td>{ exercise.unit }</td>
+            <td>{ exercise.date }</td>
+            <td>Edit</td>
+            <td>Delete</td>
         </tr>
     );
 }
