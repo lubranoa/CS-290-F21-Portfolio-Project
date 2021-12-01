afb6be8d2739ab785fe0316876982571ff118f13
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 08:06:00 -0600

    Add boilerplate code to EditExercisePage.js

---
+++ lubranoa_react/src/pages/EditExercisePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EditExercisePage = () => {
+
+    return (
+        <div>
+            <h1>Edit Exercise</h1>
+        </div>
+    );
+}
+
+export default EditExercisePage;
