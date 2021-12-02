3725f85ba6fa0ee3110d1808b5596f45ae7cfb74
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 21:53:06 -0600

    Add import and <p> calling InputTable

--- lubranoa_react/src/pages/AddExercisePage.js
+++ lubranoa_react/src/pages/AddExercisePage.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
+import InputTable from "../components/InputTable";
 
 const AddExercisePage = () => {
 
     return (
         <>
             <h2>Add Exercise</h2>
+            <p>< InputTable /></p>
         </>
     );
 }
