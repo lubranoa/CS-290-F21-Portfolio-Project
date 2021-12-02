e3f738c03b1172d0bedd105d2ebdff879545e266
lubranoa <lubranoa@oregonstate.edu>
2021-12-02 07:44:25 -0600

    Add editExercise function

--- lubranoa_react/src/pages/EditExercisePage.js
+++ lubranoa_react/src/pages/EditExercisePage.js
@@ -3,7 +3,32 @@ import { useHistory } from "react-router-dom";
 import InputTableHead from "../components/InputTableHead";
 import EditExerciseRow from "../components/EditExerciseRow";
 
-export const EditExercisePage = () => {
+export const EditExercisePage = ({ exerciseToEdit }) => {
+
+    const [name, setName] = useState(exerciseToEdit.name);
+    const [reps, setReps] = useState(exerciseToEdit.reps);
+    const [weight, setWeight] = useState(exerciseToEdit.weight);
+    const [unit, setUnit] = useState(exerciseToEdit.unit);
+    const [date, setDate] = useState(exerciseToEdit.date);
+
+    const history = useHistory();
+
+    const editExercise = async () => {
+        const editedExercise = {name, reps, weight, unit, date};
+        const response = await fetch(`/exercises/${exerciseToEdit._id}`, {
+            method: 'PUT',
+            body: JSON.stringify(editedExercise),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.status === 200) {
+            alert('Successfully edited the exercise!');
+        } else {
+            alert(`Failed to edit exercise, status code = ${response.status}`);
+        }
+        history.push('/');
+    };
 
     return (
         <>
