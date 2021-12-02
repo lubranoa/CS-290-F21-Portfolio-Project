1d6932c32f4f3c7b5b79e733b6e7c118622341a4
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 21:41:55 -0600

    Add boilerplate code for InputRow.js

---
+++ lubranoa_react/src/components/InputRow.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Input () {
+    return(
+        <tr>
+            <td>text input</td>
+            <td>number input</td>
+            <td>number input</td>
+            <td>text input</td>
+            <td>text input</td>
+        </tr>
+    );
+}
+
+export default Input;
