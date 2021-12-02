eaeedef8341c0f5b9460191f605f39fc2885eae7
lubranoa <lubranoa@oregonstate.edu>
2021-12-02 07:32:38 -0600

    Add draft dom structure to EditExercisePage.js

--- lubranoa_react/src/pages/EditExercisePage.js
+++ lubranoa_react/src/pages/EditExercisePage.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import InputTableHead from "../components/InputTableHead";
+import EditExerciseRow from "../components/EditExerciseRow";
 
-const EditExercisePage = () => {
+export const EditExercisePage = () => {
 
     return (
         <>
             <h2>Edit Exercise</h2>
+            <p>
+                <table id="edit">
+                    < InputTableHead />
+                    <tbody>
+                        < EditExerciseRow name={name} setName={setName} 
+                            reps={reps} setReps={setReps} 
+                            weight={weight} setWeight={setWeight} 
+                            unit={unit} setUnit={setUnit} 
+                            date={date} setDate={setDate} />
+                    </tbody>
+                </table>
+                <button onClick={editExercise}>Save</button>
+            </p>
         </>
     );
 }
