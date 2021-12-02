830e05c43bcb0350434caab7a79dfe5fd786cac1
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 18:22:15 -0600

    Revert routing of App.js for version ^5.3.0 of r-r-dom

--- lubranoa_react/src/App.js
+++ lubranoa_react/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AddExercisePage from "./pages/AddExercisePage";
 import EditExercisePage from "./pages/EditExercisePage";
@@ -9,13 +9,19 @@ import { useState } from "react";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" exact element={<HomePage />}></Route>
-          <Route path="/add-exercise" element={<AddExercisePage />}></Route>
-          <Route path="/edit-exercise" element={<EditExercisePage />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Router>
+        <div className="App-header">
+          <Route path="/" exact>
+            < HomePage />
+          </Route>
+          <Route path="/add-exercise">
+            < AddExercisePage />
+          </Route>
+          <Route path="/edit-exercise">
+            < EditExercisePage />
+          </Route>
+        </div>
+      </Router>
     </div>
   );
 }
