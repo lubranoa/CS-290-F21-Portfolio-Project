c294c22087f66afa541b6dab6b1485492c40b5ae
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 18:03:20 -0600

    Change single-quotes to double in App.js

--- lubranoa_react/src/App.js
+++ lubranoa_react/src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AddExercisePage from './pages/AddExercisePage';
-import EditExercisePage from './pages/EditExercisePage';
-import { useState } from 'react';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AddExercisePage from "./pages/AddExercisePage";
+import EditExercisePage from "./pages/EditExercisePage";
+import { useState } from "react";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' exact element={<HomePage />}></Route>
-          <Route path='/add-exercise' element={<AddExercisePage />}></Route>
-          <Route path='/edit-exercise' element={<EditExercisePage />}></Route>
+          <Route path="/" exact element={<HomePage />}></Route>
+          <Route path="/add-exercise" element={<AddExercisePage />}></Route>
+          <Route path="/edit-exercise" element={<EditExercisePage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
