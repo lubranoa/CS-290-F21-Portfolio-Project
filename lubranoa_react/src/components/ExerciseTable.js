912e034f0e3306fbecc22a6d871f58a39ac09e1c
lubranoa <lubranoa@oregonstate.edu>
2021-12-02 07:58:49 -0600

    Pass onEditExercise through ExerciseTable.js

--- lubranoa_react/src/components/ExerciseTable.js
+++ lubranoa_react/src/components/ExerciseTable.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ExerciseTableHead from './ExerciseTableHead';
 import Exercise from './ExerciseRow';
 
-function ExerciseTable({ exercises, onDeleteExercise }) {
+function ExerciseTable({ exercises, onDeleteExercise, onEditExercise }) {
     return (
         <table id='exercises'>
             < ExerciseTableHead />
             <tbody>
                 {exercises.map((exercise, i) => < Exercise exercise={exercise}
                     onDeleteExercise={onDeleteExercise}
+                    onEditExercise={onEditExercise}
                     key={i} />)}
             </tbody>
         </table>
