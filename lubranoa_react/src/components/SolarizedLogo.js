6235a02bdc1c1c81a01b3841ddd9322917cd7f44
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 19:05:27 -0600

    Create logo react component and delete image file

---
+++ lubranoa_react/src/components/SolarizedLogo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SolarizedLogo() {
+    return (
+        <div id='logo-container'>
+            <img className='App-logo' src='https://github.com/altercation/solarized/raw/master/img/solarized-yinyang.png' alt='Draclua theme logo' />
+        </div>
+    );
+}
+
+export default SolarizedLogo;
