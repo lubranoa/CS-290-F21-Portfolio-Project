51bbea7e5110a023b49069ea2f42d6e753ea75dc
lubranoa <lubranoa@oregonstate.edu>
2021-12-01 21:29:54 -0600

    Add boilerplate code to react component InputTable.js

---
+++ lubranoa_react/src/components/InputTable.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ExerciseTableHead from './ExerciseTableHead';
+
+function ExerciseTable({ exercises }) {
+    return (
+        <table id='exercises'>
+            < ExerciseTableHead />
+            <tbody>
+                
+            </tbody>
+        </table>
+    );
+}
+
+export default ExerciseTable;
