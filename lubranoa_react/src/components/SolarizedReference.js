06c4f17ca7bad98fa52c5409ab57d53afa28395f
lubranoa <lubranoa@oregonstate.edu>
2021-12-02 08:23:37 -0600

    Add component function to SolarizedReference.js

---
+++ lubranoa_react/src/components/SolarizedReference.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SolarizedCredit() {
+    return (
+        <p className='style-reference'>
+            Color palette based on Dracula Dark Theme for IDEs and text editors.
+        </p>
+    );
+}
+
+export default SolarizedCredit;
